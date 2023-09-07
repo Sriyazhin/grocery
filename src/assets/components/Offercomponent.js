2b27a65f93eddd7e737ac5f8d86b778332cd411a
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-09-07 19:31:17 +0530

    Update Offercomponent.js

--- src/assets/components/Offercomponent.js
+++ src/assets/components/Offercomponent.js
@@ -6,12 +6,12 @@ function Offercomponent({id,codehere,offerpercentage,value,handleOfferChange,sel
             <div className='codeandradio'>
                 <div className='code'>{codehere}</div>
                   <div className='radiobutton'>
-                    <label htmlFor={id}>
-                      <input type='radio' id={id} name="offer" value={value} onChange={handleOfferChange} checked={selected === value}></input>                    
+                    <label>
+                      <input type='radio' name="offer" value={value} onChange={handleOfferChange} ></input>                    
                     </label>
                   </div>
             </div>
-
+            
               <div className='getfifty'>{offerpercentage}</div>
               <div className='codeline'><hr></hr></div>
               <div className='codedetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fringilla pharetra, morbi nisl imperdiet id.</div>
